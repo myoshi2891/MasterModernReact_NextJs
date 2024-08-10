9e9f8e571f338d3200143a995964ec5212298204
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-10 17:14:54 +0900

    Fetching Data in a Page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,16 @@
-export default function Page() {
+export default async function Page() {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
+
 	return (
 		<div>
 			<h1>Cabins Page</h1>
+
+			<ul>
+				{data.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
