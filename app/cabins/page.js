e1f0baa56e32d4b88f4f0a9f3ea36907ed236376
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-12 11:33:15 +0900

    Sharing State Between Client and Server: The URL

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 3600;
 // export const revalidate = 15;
@@ -9,8 +10,9 @@ export const metadata = {
 	title: "Cabins",
 };
 
+export default function Page({ searchParams }) {
+	const filter = searchParams?.capacity ?? "all";
 
-export default function Page() {
 	return (
 		<div>
 			<h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,8 +26,11 @@ export default function Page() {
 				nature&apos;s beauty in your own little home away from home. The
 				perfect spot for a peaceful, calm vacation. Welcome to paradise.
 			</p>
-			<Suspense fallback={<Spinner />}>
-				<CabinList />
+			<div className="flex justify-end mb-8">
+				<Filter />
+			</div>
+			<Suspense fallback={<Spinner />} key={filter}>
+				<CabinList filter={filter} />
 			</Suspense>
 		</div>
 	);
