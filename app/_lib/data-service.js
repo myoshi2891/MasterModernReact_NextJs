1c9fd39e25205bf8a1d2233b45d2ba99e81e252c
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-13 20:56:00 +0900

    Updating the Profile Using a Server Action

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -185,7 +185,7 @@ export async function createBooking(newBooking) {
 
 /////////////
 // UPDATE
-
+/*
 // The updatedFields is an object which should ONLY contain the updated data
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
@@ -229,3 +229,5 @@ export async function deleteBooking(id) {
   }
   return data;
 }
+
+*/
