d7c33e87bbc2dec24ef25372416f3b175142f681
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-16 23:07:36 +0900

    Defining Page Title and Favicon

--- pages/cabins.js
+++ pages/cabins.js
@@ -1,17 +1,28 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
 export default function Cabins() {
+	const router = useRouter();
 	return (
-		<div>
-			<h1 className="text-4xl mb-5 text-accent-400 font-medium">
-				Our Luxury Cabins
-			</h1>
-			<p className="text-primary-200 text-lg mb-10">
-				Cozy yet luxurious cabins, located right in the heart of the
-				Italian Dolomites. Imagine waking up to beautiful mountain
-				views, spending your days exploring the dark forests around, or
-				just relaxing in your private hot tub under the stars. Enjoy
-				nature&apos;s beauty in your own little home away from home. The
-				perfect spot for a peaceful, calm vacation. Welcome to paradise.
-			</p>
-		</div>
+		<>
+			<Head>
+				<title>Cain #{router.query.cabinId} | The Wild Oasis</title>
+			</Head>
+
+			<div>
+				<h1 className="text-4xl mb-5 text-accent-400 font-medium">
+					Our Luxury Cabins
+				</h1>
+				<p className="text-primary-200 text-lg mb-10">
+					Cozy yet luxurious cabins, located right in the heart of the
+					Italian Dolomites. Imagine waking up to beautiful mountain
+					views, spending your days exploring the dark forests around,
+					or just relaxing in your private hot tub under the stars.
+					Enjoy nature&apos;s beauty in your own little home away from
+					home. The perfect spot for a peaceful, calm vacation.
+					Welcome to paradise.
+				</p>
+			</div>
+		</>
 	);
 }
--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -1,8 +1,16 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 function Cabin() {
 	const router = useRouter();
-	return <div>Cabin #{router.query.cabinId}</div>;
+	return (
+		<>
+			<Head>
+				<title>Cain #{router.query.cabinId} | The Wild Oasis</title>
+			</Head>
+			<div>Cabin #{router.query.cabinId}</div>;
+		</>
+	);
 }
 
 export default Cabin;
