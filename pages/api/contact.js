5ccce14b17ef01f96df38b19e2a85bedfc6a1cdf
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-17 15:55:18 +0900

    Handling Form Submissions

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -7,12 +7,7 @@ export default async function handler(req, res) {
 			message: "Please make a POST request",
 		});
 
-	const contactData = {
-		fullName: "john",
-		email: "test",
-		subject: "booking",
-		message: "omg!",
-	};
+	const contactData = JSON.parse(req.body);
 
 	const { error } = await supabase.from("contact").insert([contactData]);
 
